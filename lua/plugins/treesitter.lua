f6225d2f3516215344f543092b3bc05a76f35b76
starrobe <starrobe@163.com>
2023-07-27 20:12:30 +0800

    feat(treesitter): add option incremental_selection

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -7,7 +7,16 @@ return {
       ensure_installed = { "vim", "lua", "c", "cpp", "python" },
       highlight = {
         enable = true,
-      }
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
     },
     config = function(_, opts)
       require("nvim-treesitter.configs").setup(opts)
