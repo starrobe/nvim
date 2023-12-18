16f875b12a27250769a2a7e4d9344a792e71a34f
starrobe <starrobe@163.com>
2023-12-18 23:22:48 +0800

    chore(coding.lua): delete rules of autopair

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -24,22 +24,6 @@ return {
         highlight_grey = 'Comment'
       },
     },
-    config = function(_, opts)
-      local npairs = require("nvim-autopairs")
-      local Rule = require("nvim-autopairs.rule")
-
-      npairs.setup(opts)
-
-      local ts_conds = require("nvim-autopairs.ts-conds")
-
-      -- press % ==> %% only while inside a comment or string
-      npairs.add_rules({
-        Rule("%", "%", "lua")
-            :with_pair(ts_conds.is_ts_node({ "string", "comment" })),
-        Rule("$", "$", "lua")
-            :with_pair(ts_conds.is_not_ts_node({ "function" }))
-      })
-    end
   },
 
   -- 注释
