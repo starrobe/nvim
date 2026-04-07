0e558c1fbb8d221c38f9cc247e222f12198541e1
starrobe <starrobe@163.com>
2026-04-07 22:54:18 +0800

    refactor(treesitter): simplify treesitter configuration
    - Remove redundant autoindent option
    - Refactor treesitter setup with autocmd-based initialization
    - Update plugin lockfile with latest versions

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -18,10 +18,20 @@ return {
         "vim",
         "vimdoc",
       },
-      sync_intall = true,
-      highlight = {
-        enable = true,
-      },
     },
+    config = function(_, opts)
+      require("nvim-treesitter").setup()
+      require("nvim-treesitter").install(opts.ensure_installed)
+
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = opts.ensure_installed,
+        callback = function()
+          vim.treesitter.start()
+          vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+          vim.wo[0][0].foldmethod = "expr"
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
+    end,
   },
 }
