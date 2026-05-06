691827f382001b36620b4893ee3c020fd1f54f3d
starrobe <starrobe@163.com>
2026-05-06 20:06:17 +0800

    refactor: re-add noice.nvim and nui.nvim
    Reverts e21c265 which removed noice. Re-adding noice.nvim with
    lsp override presets and nui.nvim as a dependency.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -15,7 +15,9 @@ vim.pack.add({
     version = vim.version.range("1.*"),
   },
   gh("stevearc/conform.nvim"),
-  gh("lewis6991/gitsigns.nvim")
+  gh("lewis6991/gitsigns.nvim"),
+  gh("MunifTanjim/nui.nvim"),
+  gh("folke/noice.nvim")
 })
 
 vim.cmd.colorscheme("tokyonight")
@@ -88,7 +90,7 @@ require("blink.cmp").setup({
       auto_show_delay_ms = 200,
     },
   },
-  cmdline = { enabled = false },
+  -- cmdline = { enabled = false },
 })
 
 -- conform.nvim
@@ -128,12 +130,6 @@ require("snacks").setup({
     },
     icons = {
       files = { enabled = false },
-      diagnostics = {
-        Error = "󰈸 ",
-        Warn = " ",
-        Hint = " ",
-        Info = "󰌪 ",
-      },
     },
   },
   notifier = { enabled = true },
@@ -169,4 +165,28 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.wo[0][0].foldmethod = "expr"
     vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
   end,
+})
+
+-- noice
+require("noice").setup({
+  lsp = {
+    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
+      ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+    },
+  },
+  -- you can enable a preset for easier configuration
+  presets = {
+    bottom_search = true,         -- use a classic bottom cmdline for search
+    command_palette = false,      -- position the cmdline and popupmenu together
+    long_message_to_split = true, -- long messages will be sent to a split
+    inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border = false,       -- add a border to hover docs and signature help
+  },
+
+  cmdline = {
+    view = "cmdline"
+  }
 })
