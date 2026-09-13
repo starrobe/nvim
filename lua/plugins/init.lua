1758655d902f63705a34ab6d5b0052027798b3ac
starrobe <starrobe@163.com>
2026-09-13 15:41:40 +0800

    chore(noice): disable noice.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,8 +13,8 @@ vim.pack.add({
   gh("neovim/nvim-lspconfig"),
   gh("nvim-treesitter/nvim-treesitter"),
   gh("lewis6991/gitsigns.nvim"),
-  gh("MunifTanjim/nui.nvim"),
-  gh("folke/noice.nvim"),
+  -- gh("MunifTanjim/nui.nvim"),
+  -- gh("folke/noice.nvim"),
 })
 
 vim.cmd.colorscheme("tokyonight-storm")
@@ -162,39 +162,39 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 -- noice
-require("noice").setup({
-  lsp = {
-    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-    override = {
-      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-      ["vim.lsp.util.stylize_markdown"] = true,
-      ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-    },
-  },
-  -- you can enable a preset for easier configuration
-  presets = {
-    bottom_search = true, -- use a classic bottom cmdline for search
-    command_palette = true, -- position the cmdline and popupmenu together
-    long_message_to_split = true, -- long messages will be sent to a split
-    inc_rename = false, -- enables an input dialog for inc-rename.nvim
-    lsp_doc_border = false, -- add a border to hover docs and signature help
-  },
-
-  -- Neovim 0.12 emits shell_cmd/shell_out/shell_err/shell_ret for `:!` output
-  -- (msg_show.return_prompt was removed). The default routes don't match these
-  -- kinds, so `:!ls` output was swallowed. Route them to a popup to show it.
-  routes = {
-    {
-      view = "split",
-      filter = {
-        event = "msg_show",
-        kind = { "shell_cmd", "shell_out", "shell_err", "shell_ret" },
-      },
-      opts = { title = "Shell" },
-    },
-  },
-
-  -- cmdline = {
-  --   view = "cmdline"
-  -- }
-})
+-- require("noice").setup({
+--   lsp = {
+--     -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+--     override = {
+--       ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+--       ["vim.lsp.util.stylize_markdown"] = true,
+--       ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+--     },
+--   },
+--   -- you can enable a preset for easier configuration
+--   presets = {
+--     bottom_search = true, -- use a classic bottom cmdline for search
+--     command_palette = true, -- position the cmdline and popupmenu together
+--     long_message_to_split = true, -- long messages will be sent to a split
+--     inc_rename = false, -- enables an input dialog for inc-rename.nvim
+--     lsp_doc_border = false, -- add a border to hover docs and signature help
+--   },
+--
+--   -- Neovim 0.12 emits shell_cmd/shell_out/shell_err/shell_ret for `:!` output
+--   -- (msg_show.return_prompt was removed). The default routes don't match these
+--   -- kinds, so `:!ls` output was swallowed. Route them to a popup to show it.
+--   routes = {
+--     {
+--       view = "split",
+--       filter = {
+--         event = "msg_show",
+--         kind = { "shell_cmd", "shell_out", "shell_err", "shell_ret" },
+--       },
+--       opts = { title = "Shell" },
+--     },
+--   },
+--
+--   -- cmdline = {
+--   --   view = "cmdline"
+--   -- }
+-- })
