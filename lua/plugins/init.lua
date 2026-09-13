e3b4f7c79675d38f38bebe28b75490f0287c9717
starrobe <starrobe@163.com>
2026-09-13 10:07:27 +0800

    feat: enable noice.nvim (with nui.nvim)

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,8 +13,8 @@ vim.pack.add({
   gh("neovim/nvim-lspconfig"),
   gh("nvim-treesitter/nvim-treesitter"),
   gh("lewis6991/gitsigns.nvim"),
-  -- gh("MunifTanjim/nui.nvim"),
-  -- gh("folke/noice.nvim"),
+  gh("MunifTanjim/nui.nvim"),
+  gh("folke/noice.nvim"),
 })
 
 vim.cmd.colorscheme("tokyonight-storm")
@@ -161,25 +161,25 @@ vim.api.nvim_create_autocmd("FileType", {
 })
 
 -- noice
--- require("noice").setup({
---   lsp = {
---     -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
---     override = {
---       ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
---       ["vim.lsp.util.stylize_markdown"] = true,
---       ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
---     },
---   },
---   -- you can enable a preset for easier configuration
---   presets = {
---     bottom_search = true,         -- use a classic bottom cmdline for search
---     command_palette = true,      -- position the cmdline and popupmenu together
---     long_message_to_split = true, -- long messages will be sent to a split
---     inc_rename = false,           -- enables an input dialog for inc-rename.nvim
---     lsp_doc_border = false,       -- add a border to hover docs and signature help
---   },
---
---   cmdline = {
---     view = "cmdline"
---   }
--- })
+require("noice").setup({
+  lsp = {
+    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
+      ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+    },
+  },
+  -- you can enable a preset for easier configuration
+  presets = {
+    bottom_search = true,         -- use a classic bottom cmdline for search
+    command_palette = true,      -- position the cmdline and popupmenu together
+    long_message_to_split = true, -- long messages will be sent to a split
+    inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border = false,       -- add a border to hover docs and signature help
+  },
+
+  -- cmdline = {
+  --   view = "cmdline"
+  -- }
+})
